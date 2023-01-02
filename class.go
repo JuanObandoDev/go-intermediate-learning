7b958255398828640e7e7c622d9da9eed593532a
Juan Jose <juanobando.developer@gmail.com>
2023-01-01 23:13:45 -0500

    adding receiver functions, the equivalent of methods in a class in oop

--- class.go
+++ class.go
@@ -8,7 +8,27 @@ type Employee struct {
 	name string
 }
 
+// this is equivalent to a method in other languages
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) GetId() int {
+	return e.id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employee) GetName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{1, "Juan"}
 	fmt.Printf("%+v\n", e)
+	e.SetId(2)
+	e.SetName("Pedro")
+	fmt.Printf("id: %d, name: %s", e.GetId(), e.GetName())
 }
