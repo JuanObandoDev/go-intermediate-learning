f9a1b99029ebc8f267bfb57f4b3e3577f13c2bb5
Juan Jose <juanobando.developer@gmail.com>
2023-01-06 23:07:34 -0500

    testing functions in testing.go in two different ways

---
+++ testing/testing_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	total := Sum(5, 5)
+
+	if total != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+	}
+
+	// other way to do it
+	tables := []struct {
+		x int
+		y int
+		z int
+	}{
+		{1, 1, 2},
+		{2, 2, 4},
+		{3, 3, 6},
+	}
+
+	for _, item := range tables {
+		total := Sum(item.x, item.y)
+		if total != item.z {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", item.x, item.y, total, item.z)
+		}
+	}
+}
